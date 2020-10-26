aaa42d0fa699c7600f3082d656c5b5a889488e71
m.krzyszczyk <marek.krzyszczyk@wp.pl>
2020-10-26 23:20:43 +0100

    added constants

--- src/components/todo/AuthenticationService.js
+++ src/components/todo/AuthenticationService.js
@@ -1,27 +1,30 @@
 import axios from "axios";
+import {API_URL} from '../../Constants'
+
+export const USER_NAME_SESSION_ATTRIBUTE_NAME = 'authenticatedUser';
 
 class AuthenticationService {
 
     executeBasicAuthenticationService(username, password) {
-        return axios.get(`http://localhost:8080/basicauth`, {
+        return axios.get(`${API_URL}/basicauth`, {
             headers: {authorization: this.createBasicAuthToken(username, password)}
         });
     }
 
     executeJwtAuthenticationService(username, password) {
-        return axios.post(`http://localhost:8080/authenticate`, {
+        return axios.post(`${API_URL}/authenticate`, {
                  username, password
             }
         );
     }
 
     registerSuccesfullLogin(username, password) {
-        sessionStorage.setItem('authenticatedUser', username);
+        sessionStorage.setItem(USER_NAME_SESSION_ATTRIBUTE_NAME, username);
         this.setupAxiosInterceptors(this.createBasicAuthToken(username, password));
     }
 
     registerSuccessfulLoginForJwt(username, token) {
-        sessionStorage.setItem('authenticatedUser', username);
+        sessionStorage.setItem(USER_NAME_SESSION_ATTRIBUTE_NAME, username);
         this.setupAxiosInterceptors(this.createJwtAuthToken(token));
     }
 
@@ -35,16 +38,16 @@ class AuthenticationService {
     }
 
     logout() {
-        sessionStorage.removeItem('authenticatedUser');
+        sessionStorage.removeItem(USER_NAME_SESSION_ATTRIBUTE_NAME);
     }
 
     isUserLoggedIn() {
-        let user = sessionStorage.getItem('authenticatedUser');
+        let user = sessionStorage.getItem(USER_NAME_SESSION_ATTRIBUTE_NAME);
         return user !== null;
     }
 
     getLoggedInUserName() {
-        let user = sessionStorage.getItem('authenticatedUser');
+        let user = sessionStorage.getItem(USER_NAME_SESSION_ATTRIBUTE_NAME);
         if (user === null) {
             return ''
         } else return user;
